c974ed7731539b4d545e719ec06d4189f3702d9d
Dermot Haughey <hderms@gmail.com>
2026-06-02 15:26:38 -0500

    set lower min chunk size

--- demo/gradio_demo.py
+++ demo/gradio_demo.py
@@ -403,7 +403,7 @@ class VibeVoiceDemo:
             chunk_count = 0
             last_yield_time = time.time()
             min_yield_interval = 15 # Yield every 15 seconds
-            min_chunk_size = sample_rate * 30 # At least 2 seconds of audio
+            min_chunk_size = sample_rate * 10 # At least 2 seconds of audio
             
             # Get the stream for the first (and only) sample
             audio_stream = audio_streamer.get_stream(0)
